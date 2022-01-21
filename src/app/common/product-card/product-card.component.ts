a5deb3ab24610e14fccd7286c25db26c00f1d010
DiSLucky7 <nagyklevi@gmail.com>
2022-01-21 21:50:54 +0100

    product card

---
+++ src/app/common/product-card/product-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/hu';
+registerLocaleData(localeFr, 'hu');
+
+@Component({
+  selector: 'app-product-card',
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.scss']
+})
+export class ProductCardComponent implements OnInit {
+
+  @Input() product: Product = {}
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
