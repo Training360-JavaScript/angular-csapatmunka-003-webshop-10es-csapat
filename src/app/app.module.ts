9dd9d2ddf04d0ded9017ac1c69ded6fc13f9494b
DiSLucky7 <nagyklevi@gmail.com>
2022-01-26 21:10:06 +0100

    filter pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { Cat01Component } from './page/cat01/cat01.component';
 import { Cat02Component } from './page/cat02/cat02.component';
 import { HomeComponent } from './page/home/home.component';
 import { ProductCardComponent } from './common/product-card/product-card.component';
+import { FilterPipe } from './pipe/filter.pipe';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,11 +18,13 @@ import { ProductCardComponent } from './common/product-card/product-card.compone
     Cat01Component,
     Cat02Component,
     HomeComponent,
-    ProductCardComponent
+    ProductCardComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
