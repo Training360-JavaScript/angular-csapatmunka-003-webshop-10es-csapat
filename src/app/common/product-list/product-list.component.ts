cb73d9f2d7ff9b162312e87f44635adb6e0453af
AnHuszti <husztiandrea82@gmail.com>
2022-01-30 16:26:43 +0100

    add something

--- src/app/common/product-list/product-list.component.ts
+++ src/app/common/product-list/product-list.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
-
+  
   @Input() products: Product[] = []
 
   constructor() { }
